0f1dc7b56dfad27129fda8a5abbd915065e69b90
DominWos <Dom@Test.pl>
2019-01-27 14:34:48 +0100

    First Commit

---
+++ src/com/company/Ex1Map.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class Ex1Map {
+    HashMap<String, Integer> scoreBoard = new HashMap<>();
+
+    public Ex1Map() {
+        scoreBoard.put("Adam", 1);
+        scoreBoard.put("Michał", 5);
+        scoreBoard.put("Karol", 15);
+        scoreBoard.put("Adaś", 2);
+        scoreBoard.put("Lukasz ", 6);
+        scoreBoard.put("Patryk", 12);
+        scoreBoard.put("Waclaw", 23);
+        scoreBoard.put("Joachim", 23);
+        scoreBoard.put("Jozef", 65);
+        scoreBoard.put("Andrzej", 55);
+        scoreBoard.put("Jędrzej", 32);
+        scoreBoard.put("Mirosław", 5);
+        scoreBoard.put("Stanisław", 11);
+        scoreBoard.put("Patrycjusz", 43);
+        scoreBoard.put("Dawid", 15);
+    }
+}
